bf9a0ac45c8aba9679ef65a1feb3905ee08f3145
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-31 12:44:07 +0200

    feat reservation model and dummy db data

---
+++ models/Reservation.js
@@ -0,0 +1,15 @@
+const uniquid = require('uniqid')
+const fs = require('fs');
+const path = require('path');
+const dbPath = path.join(process.cwd(), 'db', 'events.json');
+
+module.exports = class Reservation {
+    id
+    constructor({ firstName, lastName, email, eventId }) {
+        this.id = uniquid();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.eventId = eventId;
+    }
+}
